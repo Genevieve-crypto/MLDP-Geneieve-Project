68f0c3d00cc9c81edd7c5fdf92c20594eac5804c
jaden <103617079+Genevieve-crypto@users.noreply.github.com>
2025-07-30 11:19:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 
 
-model = joblib.load("insurance_lr_22July.pkl")
+model = joblib.load("insurance_model_22July.pkl")
 
 # Streamlit app
 st.title("Insurance Response Prediction")
